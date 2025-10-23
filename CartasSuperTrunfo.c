fb4bcd602939a070e24b939c3fdfc2a703a61f7e
JARVIS2505 <ryangabrielleguizamon1@gmail.com>
2025-10-23 23:58:28 +0000

    Adicionar estrutura inicial e impressão das cartas do Super Trunfo

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+ int main(){
+    char estado;
+    char codigo[20];
+    char nome_da_cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int numeros_de_pontos_Turisticos;
+
+    printf("Carta 1: \n");
+    printf("Estado: A\n");
+    printf("Codigo: A01\n");
+    printf("Nome da cidade: Cuiabá\n");
+    printf("população; 650.912\n");
+    printf("Área: 3.538km²\n");
+    printf("PIB: R$ 47.700,88 bilhões\n");
+    printf("Numeros de Pontos Turisticos: 15\n\n");
+
+    char estado2;
+    char codigo2[20];
+    char nome_da_cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int numeros_de_pontos_Turisticos2;
+
+    printf("Carta : 2 \n");
+    printf("Estado: B\n", estado2);
+    printf("Codigo: B02\n", codigo2);
+    printf("Nome da cidade: Campo Grade\n", nome_da_cidade2);
+    printf("população; 898.100\n", populacao2);
+    printf("Área: 8.096km²\n", area2);
+    printf("PIB: R$ 30.121,78 bilhões\n", pib2);
+    printf("Numeros de Pontos Turisticos: 14\n\n");
+
+
+    
+ }
