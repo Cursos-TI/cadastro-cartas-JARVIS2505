22a8005f46be5d7c0b69c1146bf231d1a1fca6d5
JARVIS2505 <ryangabrielleguizamon1@gmail.com>
2025-10-25 01:36:56 +0000

    Adicionei A Densidade populacional e o PIB Per Capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
     float area;
     float pib;
     int numeros_de_pontos_Turisticos;
+    float pib_per_capita;
+    float densidade_populacional;
 
     printf("Carta 1: \n");
     printf("Estado: A\n");
@@ -15,7 +17,9 @@
     printf("população; 650.912\n");
     printf("Área: 3.538km²\n");
     printf("PIB: R$ 47.700,88 bilhões\n");
-    printf("Numeros de Pontos Turisticos: 15\n\n");
+    printf("Numeros de Pontos Turisticos: 15\n");
+    printf("Pib per capita: R$ 73.271,00\n");
+    printf("Densidade populacional: 184,02 hab/km²\n\n");
 
     char estado2;
     char codigo2[20];
@@ -24,6 +28,8 @@
     float area2;
     float pib2;
     int numeros_de_pontos_Turisticos2;
+    float pib_per_capoita2;
+    float densidade_populacional2;
 
     printf("Carta : 2 \n");
     printf("Estado: B\n", estado2);
@@ -32,7 +38,9 @@
     printf("população; 898.100\n", populacao2);
     printf("Área: 8.096km²\n", area2);
     printf("PIB: R$ 30.121,78 bilhões\n", pib2);
-    printf("Numeros de Pontos Turisticos: 14\n\n");
+    printf("Numeros de Pontos Turisticos: 14\n", numeros_de_pontos_Turisticos2);
+    printf("Pib per capita: R$ 33.540,00\n", pib_per_capoita2);
+    printf("Densidade populacional: 110,92 hab/km²\n", densidade_populacional2);
 
 
     
